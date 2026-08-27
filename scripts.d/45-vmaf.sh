b3aa40f1926d2808d700cec5936db3d6aa1e829f
Lawrence Curtis <lawrencecurtis@yellowpad.co.uk>
2026-08-27 10:56:20 +0100

    Update VMAF script repository and commit hash Update version check in ffbuild_configure for numeric comparison Change repo from Netflix to lawrencecurtis fork with latest commit

--- scripts.d/45-vmaf.sh
+++ scripts.d/45-vmaf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://github.com/Netflix/vmaf.git"
-SCRIPT_COMMIT="f85a853692a8c730d0270cd733c8bb30b5b93b7c"
+SCRIPT_REPO="https://github.com/lawrencecurtis/vmaf.git"
+SCRIPT_COMMIT="23e072f72617fb7e308b1eebcfc943836dfc336e"
 
 ffbuild_enabled() {
     return 0
@@ -69,7 +69,7 @@ ffbuild_dockerbuild() {
 }
 
 ffbuild_configure() {
-    (( $(ffbuild_ffver) >= 501 )) || return 0
+    (($(ffbuild_ffver) >= 501)) || return 0
     echo --enable-libvmaf
 }
 
